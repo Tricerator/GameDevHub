df836ed4dbfd7e270ed3618228f98a704278450f
Tran Duc Thien <56769434+trvedvc@users.noreply.github.com>
2022-03-26 20:17:11 +0100

    ani motions items

--- Player.py
+++ Player.py
@@ -5,6 +5,16 @@ UPDATES_PER_FRAME = 8
 RIGHT_FACING = 0
 LEFT_FACING = 1
 
+IDLE_TEX = 0
+WALKING_TEX = 1
+COMBAT_TEX = 2
+HOOD_IDLE_TEX = 3
+HOOD_WALKING_TEX = 4
+HOOD_COMBAT_TEX = 5
+SWORD_IDLE_TEX = 6
+SWORD_WALKING_TEX = 7
+SWORD_COMBAT_TEX = 8
+
 
 class Player(arcade.Sprite):
 
@@ -13,9 +23,10 @@ class Player(arcade.Sprite):
         self.stamina = 10
         self.max_health = 100
         self.health = self.max_health
-        self.viewP = ""
+        self.viewP = [0,0]
         self.coins = 0
 
+        '''
         self.textures = []
 
         # Load a left facing texture and a right facing texture.
@@ -28,28 +39,76 @@ class Player(arcade.Sprite):
 
         # By default, face left.
         self.texture = self.textures[0]
+        '''
         
-        
-        self.character_face_direction = RIGHT_FACING
-
-        self.idle_textures = []
-        self.walking_textures = []
-        self.combat_textures = [] # 4d pole?
+        self.character_face_direction = 0
 
         self.scale = 1
         self.cur_texture = 0
 
-        self.textures_sheet = arcade.load_spritesheet(f"images/player.png", 128, 64, 12, 60, 0)
-        for i in range(8):
-            self.idle_textures.append(self.textures_sheet[i])
-        for i in range(12, 20):
-            self.walking_textures.append(self.textures_sheet[i])
+        self.is_attacking = False
+        self.attack_texture = 0
 
-        self.texture = self.textures_sheet[0]
+        self.all_textures = []
+        for i in range(9):
+            self.all_textures.append([[]])
+
+        indexes = [0,8,4,12,12,20,16,24,24,32,28,36]
 
 
+        """
+                TEXTURY INQUISITORA PRO ANIMACI
+        """
+        pngtextury = ["inquisitor", "hood", "sword"]
+        obj = 0
+        for png in range(3):
+            self.sheet = arcade.load_spritesheet(f"images/{pngtextury[png]}.png", 128, 64, 12, 60, 0)
+            self.sheet1 = arcade.load_spritesheet(f"images/{pngtextury[png]}1.png", 128, 64, 12, 60, 0)
+            mi, ma = 0, 1
+            for j in range(3):
+                print()
+                print(mi,ma)
+                for i in range(indexes[mi], indexes[ma]):
+                    self.all_textures[obj].append([self.sheet[i]])
+                index = 0
+                del self.all_textures[obj][0]
+                mi += 2
+                ma += 2
+                print(mi, ma)
+                for i in range(indexes[mi], indexes[ma]):
+                    self.all_textures[obj][index].append(self.sheet1[i])
+                    index += 1
+                obj += 1
+                mi += 2
+                ma += 2
 
-    def move(self,key):
+            '''
+            for i in range(12, 20):
+                self.all_textures[obj].append([self.sheet[i]])
+            index = 0
+            del self.all_textures[obj][0]
+            for i in range(16, 24):
+                self.all_textures[obj][index].append(self.sheet1[i])
+                index += 1
+            obj += 1
+            for i in range(24, 32):
+                self.all_textures[obj].append([self.sheet[i]])
+            index = 0
+            del self.all_textures[obj][0]
+            for i in range(28,36):
+                self.all_textures[obj][index].append(self.sheet1[i])
+                index += 1
+            obj += 1
+            '''
+
+        self.texture = self.all_textures[IDLE_TEX][0][0]
+        self.hood = arcade.Sprite("images/hood.png")
+        self.hood.texture = self.all_textures[HOOD_IDLE_TEX][0][0]
+        self.sword = arcade.Sprite("images/sword.png")
+        self.sword.texture = self.all_textures[SWORD_IDLE_TEX][0][0]
+
+
+    def move(self, key):
 
         if key == "U":
             self.viewP = [0, 1]
@@ -59,26 +118,50 @@ class Player(arcade.Sprite):
             self.viewP = [-1, 0]
         elif key == "R":
             self.viewP = [1, 0]
-        
-        
+
+    def update(self):
+        self.hood.center_x = self.center_x
+        self.hood.center_y = self.center_y
+        self.sword.center_x = self.center_x
+        self.sword.center_y = self.center_y
 
     def update_animation(self, delta_time: float = 1 / 60):
 
+
+        direction = 0
+        if self.viewP[0] == 1:
+            direction = 1
         # Idle animation
-        if self.change_x == 0 and self.change_y == 0:
+        if not self.is_attacking:
+            if self.change_x == 0 and self.change_y == 0:
+                self.cur_texture += 1
+                if self.cur_texture > 8 * UPDATES_PER_FRAME - 1:
+                    self.cur_texture = 0
+                frame = self.cur_texture // UPDATES_PER_FRAME
+                self.texture = self.all_textures[IDLE_TEX][frame][direction]
+                self.hood.texture = self.all_textures[HOOD_IDLE_TEX][frame][direction]
+                self.sword.texture = self.all_textures[SWORD_IDLE_TEX][frame][direction]
+                return
+
+
+            # Walking animation
             self.cur_texture += 1
             if self.cur_texture > 8 * UPDATES_PER_FRAME - 1:
                 self.cur_texture = 0
-                frame = self.cur_texture // UPDATES_PER_FRAME
-                direction = self.character_face_direction
-                self.texture = self.idle_textures[frame]#[direction]
-            return
+            frame = self.cur_texture // UPDATES_PER_FRAME
+            self.texture = self.all_textures[WALKING_TEX][frame][direction]
+            self.hood.texture = self.all_textures[HOOD_WALKING_TEX][frame][direction]
+            self.sword.texture = self.all_textures[SWORD_WALKING_TEX][frame][direction]
+
+        else:
+            self.attack_texture += 1
+            if self.attack_texture > 8 * 4 - 1:
+                self.attack_texture = 0
+                self.is_attacking = False
+            frame = self.attack_texture // 4
+            self.texture = self.all_textures[COMBAT_TEX][frame][direction]
+            self.hood.texture = self.all_textures[HOOD_COMBAT_TEX][frame][direction]
+            self.sword.texture = self.all_textures[SWORD_COMBAT_TEX][frame][direction]
 
 
-        # Walking animation
-        self.cur_texture += 1
-        if self.cur_texture > 8 * UPDATES_PER_FRAME - 1:
-            self.cur_texture = 0
-        frame = self.cur_texture // UPDATES_PER_FRAME
-        direction = self.character_face_direction
-        self.texture = self.walking_textures[frame]#[direction]
+
