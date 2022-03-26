2826dc06e081d470bd9fe0edb7b8e0897614d539
Coelurus <56789091+Coelurus@users.noreply.github.com>
2022-03-26 13:03:01 +0100

    lepší zeď

--- Wall.py
+++ Wall.py
@@ -2,6 +2,8 @@ import arcade
 
 class Wall(arcade.Sprite):
 
-    def __init__(self,a,b):
+    def __init__(self,a,b, TILE_SIZE):
         super().__init__(a,b)
         self.lives = 30
+        self.width = TILE_SIZE
+        self.height = TILE_SIZE
