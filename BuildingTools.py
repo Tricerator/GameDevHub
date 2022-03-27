6e490a2fed074a35b07ee2d24d11de776889a7f9
Tricerator <37005796+Tricerator@users.noreply.github.com>
2022-03-27 13:44:21 +0200

    Update BuildingTools.py

--- BuildingTools.py
+++ BuildingTools.py
@@ -5,5 +5,5 @@ class BuildingTools(arcade.Sprite):
 
     def __init__(self, a, b, tile_size):
         super().__init__(a, b)
-        self.width = tile_size//3*2
-        self.height = tile_size//3*2
+        self.width = tile_size
+        self.height = tile_size
